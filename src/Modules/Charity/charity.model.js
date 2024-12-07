4430a5a0f5060f243adfe6855274507700b9d0a0
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-07 22:36:11 +0700

    Update charity.model.js

--- src/Modules/Charity/charity.model.js
+++ src/Modules/Charity/charity.model.js
@@ -2,6 +2,6 @@ import { getDbConnection } from '../../Config/dbConnections.js';
 
 const clusterURI = process.env.MONGO_URI;
 
-const charityDb = getDbConnection('charityDb', clusterURI);
+const charityDb = getDbConnection('charityDB', clusterURI);
 
 //something...
