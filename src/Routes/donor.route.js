256523fc41e25d4e50b0b2bfb6cef1fce1b15f41
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-07 22:39:56 +0700

    Update donor.route.js

--- src/Routes/donor.route.js
+++ src/Routes/donor.route.js
@@ -4,6 +4,6 @@ import { getAllDonors, addDonor } from '../Modules/Donor/donor.controller.js';
 const router = express.Router();
 
 router.get('/all', getAllDonors); //GET 
-router.post('/create', addDonor);    //POST 
+router.post('/create', addDonor); //POST 
 
 export default router;
