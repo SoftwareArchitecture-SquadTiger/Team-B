6974e0c825b0f2e871c20529bb2312b9376b09ad
BaoLG4 <bluong2002@gmail.com>
2024-12-09 11:22:06 +0700

    Charity Model update

--- Backend/src/Modules/Charity/charity.model.js
+++ Backend/src/Modules/Charity/charity.model.js
@@ -1,7 +1,65 @@
 import { getDbConnection } from '../../Config/dbConnections.js';
+import mongoose from 'mongoose';
+import { v4 as uuidv4 } from "uuid";
+import "dotenv/config";
 
 const clusterURI = process.env.MONGO_URI;
 
-const charityDb = getDbConnection('charityDB', clusterURI);
+// Get 'charityDB' connection
+const charityDb = getDbConnection("charityDB", clusterURI);
 
-//something...
+const charitySchema = new mongoose.Schema(
+  {
+    charity_id: {
+      type: String,
+      default: uuidv4, // Generate UUID
+      unique: true,
+      immutable: true,
+    },
+    user_id: {
+      type: String,
+      required: true, // Ownership
+    },
+    organization_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    organization_type: {
+      type: String,
+      required: true,
+      enum: ['Individual', 'Company', 'Non-Profit'], 
+    },
+    tax_code: {
+      type: String,
+      required: false, 
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    country: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    logo: {
+      type: String,
+      default: null, 
+    },
+    credit_card_info: {
+      type: String,
+      required: false, 
+      select: false, 
+    },
+  },
+  {
+    timestamps: true, 
+  }
+);
+
+const Charity = donorDb.model("Charity", charitySchema);
+
+export default Charity;
