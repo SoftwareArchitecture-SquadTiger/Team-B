a56663ee79b1524352788dd48bf7483223600c27
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-10 00:48:30 +0700

    Setting Up file structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Your React App</h1>
+        <p>Start developing your application by editing <code>App.jsx</code>.</p>
+      </header>
+    </div>
+  );
+};
+
+export default App
