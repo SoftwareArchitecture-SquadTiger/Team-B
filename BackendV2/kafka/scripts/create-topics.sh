acfc90d5437818bb627ffa458e26a4523dfd0a4d
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-15 11:22:32 +0700

    Delete create-topics.sh

--- BackendV2/kafka/scripts/create-topics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wait for Kafka to be ready
-sleep 10
-
-# Create topics
-kafka-topics.sh --create --topic my-topic-1 --bootstrap-server localhost:9092 --partitions 3 --replication-factor 1
-kafka-topics.sh --create --topic my-topic-2 --bootstrap-server localhost:9092 --partitions 2 --replication-factor 1
