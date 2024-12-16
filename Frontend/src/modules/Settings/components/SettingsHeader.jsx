0866f4724ec55e9541f84f59aca5a34fc2a80e94
luandang2003 <mrlonelymo2003@gmail.com>
2024-12-17 00:41:52 +0700

    Create SettingsHeader.jsx

---
+++ Frontend/src/modules/Settings/components/SettingsHeader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SettingsHeader = () => {
+  return (
+    <div className="flex justify-between items-center mb-6">
+      <h2 className="text-2xl font-semibold text-gray-800">Settings</h2>
+    </div>
+  );
+};
+
+export default SettingsHeader;
