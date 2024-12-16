7a28f9437b1107d9afb34c9ff60d1279cdf14ef8
luandang2003 <mrlonelymo2003@gmail.com>
2024-12-17 00:32:40 +0700

    added components signup and submit button for signinpage

--- Frontend/src/modules/Auth/components/SignupPrompt.jsx
+++ Frontend/src/modules/Auth/components/SignupPrompt.jsx
@@ -1,17 +1,17 @@
-// import React from "react";
+import React from "react";
 
-// const SignupPrompt = ({ onClick }) => {
-//   return (
-//     <p className="text-sm text-gray-600 text-center mt-4">
-//       Don’t have an account?{" "}
-//       <button
-//         onClick={onClick}
-//         className="text-pink-500 font-medium hover:underline bg-transparent border-none cursor-pointer"
-//       >
-//         Sign up.
-//       </button>
-//     </p>
-//   );
-// };
+const SignupPrompt = ({ onClick }) => {
+  return (
+    <p className="text-sm text-gray-600 text-center mt-4">
+      Don’t have an account?{" "}
+      <button
+        onClick={onClick}
+        className="text-pink-500 font-medium hover:underline bg-transparent border-none cursor-pointer"
+      >
+        Sign up.
+      </button>
+    </p>
+  );
+};
 
-// export default SignupPrompt;
+export default SignupPrompt;
