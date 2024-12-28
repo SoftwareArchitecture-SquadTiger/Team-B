340f58a2b12b4a151f7a50121f667c8141be3693
Vally <113294564+Needalife@users.noreply.github.com>
2024-12-28 14:00:00 +0700

    add subscription services

--- BackendV2/services/donor-service/src/entities/subscription/subscription.service.js
+++ BackendV2/services/donor-service/src/entities/subscription/subscription.service.js
@@ -9,7 +9,22 @@ const getEmailsByCategories = async (categories) => {
     return await subscriptionRepository.getByCategories(categories);
 };
 
+const createSubscriptions = async (subscriptionData) => {
+    return await subscriptionRepository.create(subscriptionData);
+};
+
+const updateSubscriptions = async (email, subscriptionData) => {
+    return await subscriptionRepository.update(email, subscriptionData);
+};
+
+const clearSubscriptions = async (email) => {
+    return await subscriptionRepository.remove(email)
+};
+
 export default {
     getSubscriptionsByEmail,
     getEmailsByCategories,
+    createSubscriptions,
+    updateSubscriptions,
+    clearSubscriptions,
 }
--- BackendV2/services/donor-service/src/utils/actionHandler.js
+++ BackendV2/services/donor-service/src/utils/actionHandler.js
@@ -1,47 +1,67 @@
-import donorService from '../entities/donor/donor.service.js';
+import donorService from "../entities/donor/donor.service.js";
 import subscriptionService from "../entities/subscription/subscription.service.js";
 
 export const actionHandlers = {
   // ======================= Donor Handlers =======================
   GET_ALL: async () => {
     const donors = await donorService.getAllDonors();
-    return { status: 'success', data: donors };
+    return { status: "success", data: donors };
   },
   GET_BY_ID: async (data) => {
     const donor = await donorService.getDonorById(data.id);
-    return { status: 'success', data: donor ? donor.toJSON() : null };
+    return { status: "success", data: donor ? donor.toJSON() : null };
   },
   GET_BY_CATEGORIES: async (data) => {
-    const donors = await donorService.getDonorsBySubscribedCategories(data.categories);
-    return { status: 'success', data: donors };
+    const donors = await donorService.getDonorsBySubscribedCategories(
+      data.categories
+    );
+    return { status: "success", data: donors };
   },
   GET_BY_REGIONS: async (data) => {
-    const donors = await donorService.getDonorsBySubscribedRegions(data.regions);
-    return { status: 'success', data: donors };
+    const donors = await donorService.getDonorsBySubscribedRegions(
+      data.regions
+    );
+    return { status: "success", data: donors };
   },
   ADD: async (data) => {
     const newDonor = await donorService.addDonor(data);
-    return { status: 'success', data: newDonor.toJSON() };
+    return { status: "success", data: newDonor.toJSON() };
   },
   UPDATE: async (data) => {
     const updatedDonor = await donorService.updateDonor(data.id, data.update);
-    return { status: 'success', data: updatedDonor.toJSON() };
+    return { status: "success", data: updatedDonor.toJSON() };
   },
   DELETE: async (data) => {
     await donorService.deleteDonor(data.id);
-    return { status: 'success', message: 'Donor deleted successfully' };
+    return { status: "success", message: "Donor deleted successfully" };
   },
   // =================== Subscription Handlers ===================
   GET_SUBSCRIPTIONS_BY_EMAIL: async (data) => {
-    const subscriptions = await subscriptionService.getSubscriptionsByEmail(data.email);
-    return { status: 'success', data: subscriptions };
+    const subscriptions = await subscriptionService.getSubscriptionsByEmail(
+      data.email
+    );
+    return { status: "success", data: subscriptions };
   },
   GET_EMAILS_BY_CATEGORIES: async (data) => {
-    const emails = await subscriptionService.getEmailsByCategories(data.categories);
-    return { status: 'success', data: emails };
+    const emails = await subscriptionService.getEmailsByCategories(
+      data.categories
+    );
+    return { status: "success", data: emails };
+  },
+  CREATE_SUBSCRIPTION: async (data) => {
+    const newSubscription = await subscriptionService.createSubscriptions(data);
+    return { status: "success", data: newSubscription };
+  },
+  UPDATE_SUBSCRIPTION: async (data) => {
+    const updatedSubscriptions = await subscriptionService.updateSubscriptions(data.email, data.update);
+    return { status: "success", data: updatedSubscriptions.toJSON() };
+  },
+  CLEAR_SUBSCRIPTION: async (data) => {
+    await subscriptionService.clearSubscriptions(data.email);
+    return { status: "success", message: "Subscription clear!" };
   },
 };
 
 export const defaultHandler = async () => {
-  return { status: 'error', message: 'Unknown action' };
+  return { status: "error", message: "Unknown action" };
 };
