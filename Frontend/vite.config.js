f9b3478591d21975ddabadc03d3d01fb7648dc09
Vally <113294564+Needalife@users.noreply.github.com>
2025-01-14 11:35:13 +0700

    up date package

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    host: '192.168.0.103',
+    host: '192.168.1.10',
   },
   plugins: [react()],
 })
